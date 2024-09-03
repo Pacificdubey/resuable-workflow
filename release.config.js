18cb9678570776fa003719dc4714a8d773918ab0
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-09-03 22:56:15 +0530

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  branches: ["main"],
+  branches: ['main'],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/git",
-    [
-      "@semantic-release/exec", {
-        "publishCmd": "echo ${{nextRelease.version}} > .version"
-    }]
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    {
+      path: '@semantic-release/git',
+      assets: ['CHANGELOG.md', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]'
+    }
   ]
 };
+
